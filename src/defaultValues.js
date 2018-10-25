41c75e04aae5a8b0e39f22b0041d0124d97e64a4
Adriano Oliveira <adriano@wsmga-adriano.objectivemga>
2018-10-25 18:00:23 -0300

    Refactoring para melhorar orientação a objetos

---
+++ src/defaultValues.js
@@ -0,0 +1,9 @@
+const DEFAULT_MASK_CONFIG={
+    mask: "(99) 9999-9999", validationCallback: (inputValue) => true , isMoney: false, moneyCountryMask: 'R$'
+}
+
+const DEFAULT_OPTIONS={
+  submitWithMask: true,
+  reverseInput: false,
+  masks: [DEFAULT_MASK_CONFIG]
+}
