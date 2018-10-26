3ff1673eadc0d1b0793274265a5f015bc65d12ce
Adriano Oliveira <adriano@wsmga-adriano.objectivemga>
2018-10-26 18:36:30 -0300

    Adicionando loaders ao webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,27 @@ module.exports = {
   output: {
     path: path.resolve('dist'),
     filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      }
+    ]
   }
 }
