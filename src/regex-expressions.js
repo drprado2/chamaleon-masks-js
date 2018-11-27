af8a36d8f230d03b199a87c748e1607b047d348e
Adriano Oliveira <adriano@wsmga-adriano.objectivemga>
2018-11-27 08:52:51 -0200

    Resolvido problema com mascara limitada

--- src/regex-expressions.js
+++ src/regex-expressions.js
@@ -2,7 +2,7 @@ const regexExpressions = {
   ONLY_DIFF_NUMBER : /[^0-9]/gm,
   START_VALUE_NOT_NUMBER : /^[^0-9]$/g,
   RANGE_PARTS: /([0-9\*]*\[\d*-\d*\])/g,
-  RANGE_VALUE: /^[0-9]*(\[\d*-\d*\])/gm,
+  RANGE_VALUE: /^[0-9]*(\[\d*-\d*\])/m,
   RANGES: /\[\d*-\d*\]/g,
   NOT_INFINIT_CHAR: /[^\*]/,
   INFINIT_CHAR: /[\*]/
